a300dca821a49c580fcb4b6bf835446433e0a1a3
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-02-01 11:39:59 +0100

    update

--- EngitelExam.Web/Models/ViewModels/Step1FamigliaVM.cs
+++ EngitelExam.Web/Models/ViewModels/Step1FamigliaVM.cs
@@ -8,12 +8,15 @@ namespace EngitelExam.Web.Models.ViewModels
 {
     public class Step1FamigliaVM
     {
+        [Display(Name = "Nome della famiglia")]  //renderizzato da LabelFor in view razor (Step1.cshtml)
         [Required, StringLength(50)]
         public string NomeFamiglia { get; set; }
 
+        [Display(Name ="Numero componenti della famiglia")]
         [Range(1, 10)]
         public int NumeroComponenti { get; set; }
 
+        [Display(Name ="Numero componenti che possiedono una macchina")]
         [Range(0, 10)]
         public int NumeroComponentiWCar { get; set; }
 
